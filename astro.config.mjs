0182fa5d1f18eb1c0c5d248a34e34c2bf8c74ac1
gingadevs <dbureos@ginga.com.mx>
2023-01-10 17:41:03 -0600

    testing action url trigger

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dev-world.github.io',
-    base: '/dev-world'
+    site: 'https://daveinwords.github.io',
+    base: 'dev-world.github.io/'
 });
