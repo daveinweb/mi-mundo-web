47998bae66fdadcc42c463939e4f048f99091fa3
gingadevs <dbureos@ginga.com.mx>
2023-01-10 17:47:51 -0600

    testing action username repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+// import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://daveinwords.github.io',
-    base: '/dev-world.github.io/'
-});
+// export default defineConfig({
+//     site: 'https://daveinwords.github.io',
+//     base: '/dev-world.github.io/'
+// });
