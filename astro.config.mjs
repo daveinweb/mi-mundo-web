b7bde8bc430161cae4adf78dec0b43a98f42289b
gingadevs <dbureos@ginga.com.mx>
2023-01-11 14:09:28 -0600

    reset

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://daveinwords.github.io',
-    base: '/mi-mundo-web/'
+    base: '/mi-mundo-web'
 });
